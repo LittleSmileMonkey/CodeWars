80945a9f1e87da8356cf0e6dfd7cc42e93b9f330
Sleep <sleepym09@163.com>
2018-06-25 09:47:24 +0800

    二叉树镜像对称检测方法（递归）

---
+++ src/com/sleep/codewars/arithmetic/binaryTree/TreeMirrorChcek.java
@@ -0,0 +1,30 @@
+package com.sleep.codewars.arithmetic.binaryTree;
+
+import com.sleep.codewars.arithmetic.TreeNode;
+
+/**
+ * Created by Sleep on 2018/6/25.
+ * <p>
+ * E-mail: sleepym09@163.com
+ */
+class TreeMirrorChcek {
+
+    //递归解法
+    static boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        else if (right == null) {
+            return false;
+        } else if (left == null) {
+            return false;
+        } else {
+            if (left.val != right.val) return false;
+
+            return isMirror(left.right, right.left) && isMirror(left.left, right.right);
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return isMirror(root.left, root.right);
+    }
+}
